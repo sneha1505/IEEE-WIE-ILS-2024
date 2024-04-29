eecc275b5dec3ea0453ff5846bf266a5fbe99bc4
Nidhish Singh <116066599+nidhishs91@users.noreply.github.com>
2024-04-29 17:56:48 +0530

    Update ticket.js

--- components/ticket.js
+++ components/ticket.js
@@ -20,7 +20,7 @@ const Track = () => {
                             <p className="text-center font-bold font-outline-10 text-[#74064a] text-4xl"> IEEE <br/>STUDENT <br/> MEMBER
                             </p>
                             <p className="text-center font-bold font-outline-10 text-4xl text-[#74064a]">₹2500</p>
-                            <p className="text-center font-bold -mx-6 rounded-b-3xl bg-pink-900 mt-10 py-5  ">BUY</p>
+                            <p className="text-center font-bold -mx-6 rounded-b-3xl bg-pink-900 mt-10 py-5 px-16">BUY</p>
                         </div>
 
                         <div id="new"
